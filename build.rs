cb2ddbcb297c2e5a61c854df5b775201bf4fc774
Milot Mirdita <milot@mirdita.de>
2025-06-12 15:43:15 +0900

    Add static binary upload

--- build.rs
+++ build.rs
@@ -1,12 +1,5 @@
-
-// #[cfg(feature = "foldcomp")] 
-// extern crate bindgen;
 #[cfg(feature = "foldcomp")] 
 extern crate cmake;
-
-// commented out together with bindgen
-//use std::env;
-//use std::path::PathBuf;
 
 fn main() {
     #[cfg(feature = "foldcomp")] 
@@ -18,7 +11,6 @@ fn main() {
 
     println!("cargo:rustc-link-search=native={}/build", dst.display());
     println!("cargo:rustc-link-lib=static=foldcomp_ffi");
-    // println!("cargo:rustc-link-lib=c++"); // TODO: FIXME
 
     // Determine the platform and link the appropriate C++ standard library
     if cfg!(target_os = "windows") {
@@ -28,27 +20,23 @@ fn main() {
         println!("cargo:rustc-link-lib=static=foldcomp_ffi");
         println!("cargo:rustc-link-lib=dylib=c++");
     } else if cfg!(target_os = "linux") {
-        // Detect the C++ compiler used by CMake and link the appropriate standard library
-        let cxx_compiler = std::env::var("CXX").unwrap_or_else(|_| "c++".to_string());
-        if cxx_compiler.contains("clang") {
-            println!("cargo:rustc-link-lib=static=foldcomp_ffi");
-            println!("cargo:rustc-link-lib=dylib=c++");
-        } else {
-            println!("cargo:rustc-link-lib=static=foldcomp_ffi");
-            println!("cargo:rustc-link-lib=dylib=stdc++");
+        println!("cargo:rustc-link-lib=static=foldcomp_ffi");
+        let link_type = std::env::var("FOLDCOMP_STD_LINK")
+            .unwrap_or_else(|_| "dylib".into());
+        let stdlib = std::env::var("FOLDCOMP_STD_LIB").unwrap_or_else(|_| {
+            // Detect the C++ compiler used by CMake and link the appropriate standard library
+            let cxx = std::env::var("CXX").unwrap_or_else(|_| "c++".into());
+            if cxx.contains("clang") {
+                "c++".into()
+            } else {
+                "stdc++".into()
+            }
+        });
+        println!("cargo:rustc-link-lib={}={}", link_type, stdlib);
+        if let Ok(dir) = std::env::var("FOLDCOMP_LIB_DIR") {
+            println!("cargo:rustc-link-search=native={}", dir);
         }
     }
-
-    // Generate bindings. If regeneration is needed, uncomment the following code
-    // let bindings = bindgen::Builder::default()
-    //     .header("lib/foldcomp/foldcompffi.h")
-    //     .generate()
-    //     .expect("Unable to generate bindings");
-
-    // let out_path = PathBuf::from("lib/foldcomp");
-    // bindings
-    //     .write_to_file(out_path.join("bindings.rs"))
-    //     .expect("Couldn't write bindings!");
 
     }
 }

db7f503c5ba299e363824bd11b145c793d55a3d7
Hyunbin Kim <khb7840@gmail.com>
2025-08-17 22:48:42 +0900

    Removed '-modified' from version string with uncommitted changes

--- src/cli/main.rs
+++ src/cli/main.rs
@@ -9,7 +9,7 @@
 use folddisco::cli::{workflows::{build_index, benchmark, query_pdb}, *};
 use git_version::git_version;
 
-const VERSION_STRING: &str = git_version!(args = ["--abbrev=40", "--always", "--dirty=-modified"]);
+const VERSION_STRING: &str = git_version!(args = ["--abbrev=40", "--always"]);
 const HELP: &str = "\
 usage: folddisco <command> [<args>]
 

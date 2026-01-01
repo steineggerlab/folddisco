1cc53f93fb59e1b61964683d8d93fd51f7d686dc
Jaewon Yoon <jwyoon05@snu.ac.kr>
2026-01-01 18:17:57 +0900

    Adjust lam calculation in result.rs

--- src/controller/result.rs
+++ src/controller/result.rs
@@ -416,7 +416,7 @@ pub fn evalue_fitting_new(x: f32, m: f32, l: f32) -> f32 {
     let l_d = l as f64;
 
     let mu = 0.01115566 * l_d.powi(2) + 0.06267775 * l_d + 18.8132141;
-    let lam = 0.53 * (-0.045 * l_d).exp();
+    let lam = 0.50 * (-0.043 * l_d).exp();
     
     let ref_db_size = 10547.0; 
     let search_space_ref = ref_db_size;
@@ -436,7 +436,7 @@ pub fn evalue_fitting_new_sat(x: f32, m: f32, l: f32) -> f32 {
     let l_d = l as f64;
 
     let mu = 0.01115566 * l_d.powi(2) + 0.06267775 * l_d + 18.8132141;
-    let lam = 0.67754035 * (-0.05775654 * l_d).exp();
+    let lam = 0.50 * (-0.043 * l_d).exp();
     
     let ref_db_size = 10547.0; 
     let param = 42.0;
